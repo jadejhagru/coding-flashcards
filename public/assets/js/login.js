5c11cfbe410995324187dad395ab08fb56844119
Jade Jhagru <j.jhagru@hotmail.com>
2021-05-27 20:11:04 -0400

    added js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,13 +1,39 @@
-const pass_field = document.querySelector('.password');
-const show_btn = document.querySelector('.show')
-show_btn.addEventListener('click', function(){
-    if(pass_field.type === "password"){
-        pass_field.type = "text";
-        show_btn.style.color = "#3498db";
-        show_btn.textContent = "HIDE";
-    }else{
-        pass_field.type = "password";
-        show_btn.style.color = "#222";
-        show_btn.textContent = "SHOW"
+  // Getting references to our form and inputs
+  var loginForm = $("#login");
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input[type='password']");
+
+  // When the form is submitted, we validate there's an email and password entered
+  loginForm.on("submit", function(event) {
+    event.preventDefault();
+    var userData = {
+      email: emailInput.val().trim().toLowerCase(),
+      password: passwordInput.val().trim()
+    };
+
+    if (!userData.email || !userData.password) {
+      return;
     }
-});
+
+    // If we have an email and password we run the loginUser function and clear the form
+    loginUser(userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
+  });
+
+  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+  function loginUser(email, password) {
+    $.post("/api/login", {
+      email: email,
+      password: password
+    }).then(function(data) {
+      window.location.replace(data);
+      console.log(data);
+      // If there's an error, log the error
+    }).catch(function(err) {
+      if (err){
+        somethingWentWrong();
+      }
+      console.log(err);
+    });
+  }
