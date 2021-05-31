2cc363b643c9d89a8b1b0f5668f3acb0a056da9a
Rani Saad <rsaad@live.ca>
2021-05-31 18:51:34 -0400

    Changes made to package.json

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const { auth, requiresAuth } = require("express-openid-connect");
 const express = require("express");
+const exphbs = require("express-handlebars");
 require("dotenv").config();
 
 const app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.use(
   auth({
