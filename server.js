3a3e165578f8fd003a6961f18ddb463d75eddb73
Rani Saad <rsaad@live.ca>
2021-06-01 01:28:25 -0400

    New branch, authentication added

--- server.js
+++ server.js
@@ -1,14 +1,79 @@
 const { auth, requiresAuth } = require("express-openid-connect");
 const express = require("express");
+const path = require("path");
+
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
 require("dotenv").config();
+
+const authRouter = require("./auth");
 
 const app = express();
 
-app.use(express.json());
+//Session Configuration
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+};
 
-app.use(express.urlencoded({ extended: true }));
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
 
-app.use(express.static("public"));
+//Passport Configuration
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL,
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
+//App configuration
+//app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+
+// Router mounting
+app.use("/", authRouter);
+
+//app.use(express.static("public"));
 
 // app.use(
 //   auth({
