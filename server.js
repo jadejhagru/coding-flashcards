9f090bc2964d2daacf73c777250d7404a500122a
Rani Saad <rsaad@live.ca>
2021-05-31 20:42:09 -0400

    Commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
